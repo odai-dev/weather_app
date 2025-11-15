42bc03dd7aaf652b79eeb68a6f86a7c531b1d845
Odai Gubran <95129948+odai-dev@users.noreply.github.com>
2025-11-15 20:02:25 +0300

    fetch weather data and extract needed info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+
+
+async function getWeather(location = {}) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=PHZ8Y8U6SV33HGUPQP4UBV99B`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+const weatherJson =  await getWeather('sana\'a');
+
+console.log(refineWeatherJson(weatherJson));
+
+async function refineWeatherJson(weatherJson = {}) {
+    return {
+        adress: weatherJson.resolvedAddress , 
+        temp: weatherJson.currentConditions.temp , 
+        desc: weatherJson.description
+    };
+}
+
