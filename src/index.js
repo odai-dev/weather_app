fd711895adb84ca281dadd3cbe8682a03417b6be
Odai Gubran <95129948+odai-dev@users.noreply.github.com>
2025-11-15 21:14:04 +0300

    cleane the error handling logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import './styles.css';
 
 
-async function getWeather(location = {}) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=PHZ8Y8U6SV33HGUPQP4UBV99B`);
-    const data = await response.json();
-    console.log(data);
-    return data;
+async function getWeather(location = '') {
+    try{
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=PHZ8Y8U6SV33HGUPQP4UBV99B&unitGroup=uk`);
+       
+        if(!response.ok) {
+            throw new Error(`API Error: ${response.status}`);
+        }
+
+        return await response.json();
+
+    } catch(e) {
+        console.error(`Fetch error: ${e}`);
+    }
 }
-const weatherJson =  await getWeather('sana\'a');
+const weatherJson =  await getWeather('ndon');
 
-console.log(refineWeatherJson(weatherJson));
+if (weatherJson) {
+    console.log(refineWeatherJson(weatherJson));
+} 
 
-async function refineWeatherJson(weatherJson = {}) {
+function refineWeatherJson(weatherJson = {}) {
     return {
         adress: weatherJson.resolvedAddress , 
-        temp: weatherJson.currentConditions.temp , 
-        desc: weatherJson.description
-    };
+        temp: weatherJson.currentConditions.temp,
+        condition: weatherJson.currentConditions.condition, 
+        desc: weatherJson.description,
+        icon: weatherJson.currentConditions.icon,
+        humidity: weatherJson.currentConditions.humidity,
+        days: weatherJson.days
+    }
 }
 
